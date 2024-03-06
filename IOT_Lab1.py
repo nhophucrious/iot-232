8688b97321c6362a9f4b0f0be7a5d8b8644be95b
Phuc Nguyen Nho Gia <nngiaphuc0101@gmail.com>
2024-03-06 23:35:54 +0700

    fix: fixed issues with lab1 and dotenv

--- IOT_Lab1.py
+++ IOT_Lab1.py
@@ -1,24 +1,36 @@
 import sys
 import os # to fix SSL cert
 from Adafruit_IO import MQTTClient
+from dotenv import load_dotenv
+# import ssl
+
+import time
+import random
 
 # os.environ['REQUESTS_CA_BUNDLE'] = '/usr/local/etc/openssl@1.1/cert.pem'
 os.environ['SSL_CERT_FILE'] = '/etc/ssl/cert.pem'
 
-import ssl
-print(ssl.get_default_verify_paths())
+# print(ssl.get_default_verify_paths())
 
-AIO_FEED_ID = "sensor1"
+AIO_FEED_IDS = [
+    "button1",
+    "button2",
+]
+
+AIO_USERNAME = ""
+AIO_KEY = ""
+
 # read from .env
-with open(".env") as f:
-    lines = f.readlines()
-    AIO_USERNAME = lines[0].strip()
-    AIO_KEY = lines[1].strip()
+load_dotenv()
+AIO_USERNAME = os.getenv("AIO_USERNAME")
+AIO_KEY = os.getenv("AIO_KEY")
+print(AIO_USERNAME, AIO_KEY)
 
 
 def connected(client):
     print("Connected successfully!")
-    client.subscribe(AIO_FEED_ID)
+    for AIO_FEED_ID in AIO_FEED_IDS:
+        client.subscribe(AIO_FEED_ID)
 
 def subscribe(client , userdata , mid , granted_qos):
     print("Subcribed successfully")
@@ -28,15 +40,43 @@ def disconnected(client):
     sys.exit (1)
 
 def message(client , feed_id , payload):
-    print("Received: " + payload)
+    print("Received: " + payload + " from " + feed_id)
 
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
-client.on_connect = connected
+try:
+    client = MQTTClient(AIO_USERNAME, AIO_KEY)
+except Exception as e:
+    print("Some shit happened: ", e)
+
+client.on_connect = connected # assign call-back to object
 client.on_disconnect = disconnected
 client.on_message = message
 client.on_subscribe = subscribe
 client.connect()
-client.loop_background()
+client.loop_background() # loop foreground blocks everything...
 
+counter = 10
+my_type = 0
 while True:
+    counter -= 1
+    if counter <= 0:
+        counter = 10
+
+        print("publishing random stuff")
+        if my_type == 0:
+            print("sending temperature data...")
+            temperature = random.randint(10, 40)
+            client.publish("sensor1", temperature)
+            my_type = 1
+        elif my_type == 1:
+            print("sending light data...")
+            light = random.randint(100, 500)
+            client.publish("sensor2", light)
+            my_type = 2
+        else:
+            print("sending humidity data...")
+            humidity = random.randint(60, 100)
+            client.publish("sensor3", humidity)
+            my_type = 0
+    # sleep for one second
+    time.sleep(1) 
     pass
--- simple_ai.py
+++ simple_ai.py
@@ -17,7 +17,7 @@ class_names = open("labels.txt", "r").readlines()
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open("/Users/phucnguyen/Desktop/test_photo.jpg").convert("RGB")
+image = Image.open("/Users/phucnguyen/Desktop/test2.jpg").convert("RGB")
 
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
